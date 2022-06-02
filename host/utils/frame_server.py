a91ed23fbe029db8eece35106c365b0eb565c6ed
Alind Xhyra <xhyra.alind@gmail.com>
2022-06-02 17:20:19 +0200

    Refactored frame_server.py

--- host/utils/frame_server.py
+++ host/utils/frame_server.py
@@ -1,3 +1,4 @@
+import logging
 import pickle
 import socketserver
 from typing import Callable, Tuple
@@ -7,9 +8,7 @@ import numpy as np
 import torch
 from torchvision import transforms
 
-from host.autoencoder import Autoencoder
 from host.obstacle_avoidance.obstacle_avoidance import ObstacleAvoidance
-import logging
 
 HEADERSIZE = 10
 
@@ -18,27 +17,19 @@ class FrameServer(socketserver.BaseServer):
     def __init__(self,
                  server_address: Tuple[str, int],
                  RequestHandlerClass: Callable[..., socketserver.StreamRequestHandler]):
-        """
-        The function takes two arguments, a tuple of strings and an integer, and a callable object that takes any number of
-        arguments and returns a StreamRequestHandler object.
-
-        :param server_address: The address on which the server will listen for incoming connections
-        :type server_address: Tuple[str, int]
-        :param RequestHandlerClass: This is the class that will handle the requests
-        :type RequestHandlerClass: Callable[..., socketserver.StreamRequestHandler]
-        """
         super().__init__(server_address, RequestHandlerClass)
 
 
 def pack_response(code: int, data: bytes):
     """
-    It takes a code and a data, and returns a response
+    It takes a return code and a data payload, and returns a response
+    packet with the return code and data length in the header
 
     :param code: the return code of the request
     :type code: int
     :param data: the data to be sent
     :type data: bytes
-    :return: The return value is a byte string that contains the response code, the length of the data, and the data itself.
+    :return: The return code and the length of the data.
     """
     res = bytes(f'{code:<{HEADERSIZE}}', 'utf-8') + \
           bytes(f'{len(data):<{HEADERSIZE}}', 'utf-8') + \
@@ -61,7 +52,7 @@ class FrameHandler(socketserver.StreamRequestHandler):
     def handle(self):
         """
         It receives a request from the client, processes it, and sends a response back
-        :return: The response is a number that represents the movement of the object.
+        :return: The response is the predicted movement of the object in the frame.
         """
         logging.info(f'Got connection from {self.client_address}')
         msg = self.rfile.readline().split()
@@ -74,20 +65,19 @@ class FrameHandler(socketserver.StreamRequestHandler):
         frame = cv2.imdecode(pickle.loads(content), cv2.IMREAD_COLOR)
         cv2.imwrite('frame.png', frame)
         frame = self.server.transform(frame).to('cuda')
-        logging.info(frame.shape)
 
         if req == 'get_movement':
             response = self.server.model(torch.unsqueeze(frame, 0))
             response = torch.softmax(response.cpu().detach(), axis=1)
             response = response.argmax(axis=1)
             response = response.item()
-            logging.info(response)
+            logging.info(str(response))
             self.wfile.write(pack_response(200, str(response).encode('utf-8')))
 
         elif req == 'get_anomaly_map':
             self.wfile.write(pack_response(404, b'Not Found'))
-            self.server.autoencoder.eval()
-            _, output = self.server.autoencoder(torch.unsqueeze(frame, 0))
+            self.server.model.eval()
+            _, output = self.server.model(torch.unsqueeze(frame, 0))
             anomaly_map = np.moveaxis(
                 (np.clip(frame.cpu().detach().numpy() - output[0].cpu().detach().numpy(), 0, 1) * 255).astype(np.uint8),
                 0, -1)
@@ -103,17 +93,15 @@ class FrameHandler(socketserver.StreamRequestHandler):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     frame_server = socketserver.ThreadingTCPServer(('0.0.0.0', 5555), FrameHandler)
-    autoencoder = Autoencoder((128, 128), 32, (128, 128),
-                              convolutional=True, dropout_rate=0,
-                              bottleneck_activation=None).to('cuda')
-    autoencoder.load_state_dict(torch.load('autoencoder.pt'))
-    autoencoder.eval()
-    
+    # model = Autoencoder((128, 128), 32, (128, 128),
+    #                     convolutional=True, dropout_rate=0,
+    #                     bottleneck_activation=None).to('cuda')
+    # model = ResNet50ViT(img_dim=128, pretrained_resnet=False, blocks=4,
+    #                     num_classes=3, dim_linear_block=64, dim=256).to('cuda')
     model = ObstacleAvoidance().to('cuda')
     model.load_state_dict(torch.load('model.pt'))
     model.eval()
     frame_server.model = model
-    frame_server.autoencoder = autoencoder
     frame_server.transform = transforms.ToTensor()
     try:
         logging.info("Started server...")
